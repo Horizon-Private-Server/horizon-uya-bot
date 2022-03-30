0e1f34795e9e32c897f91d93e8a124f96e470a4f
John Janecek <janecektyler@gmail.com>
2022-03-29 21:38:40 -0600

    Fix godbot

--- model/bots/botg.py
+++ model/bots/botg.py
@@ -13,5 +13,20 @@ class botg(prototype):
         self.game_state.player.reset_health()
         self._weapons_enabled = True
 
+        self.arsenal.weapons['flux']['shoot_rate'] = 1
+        self.arsenal.weapons['rocket']['shoot_rate'] = 1
+        self.arsenal.weapons['grav']['shoot_rate'] = 1
+        self.arsenal.weapons['n60']['shoot_rate'] = 1
+
+        self.arsenal.weapons['flux']['hit_rate'] = 1
+        self.arsenal.weapons['rocket']['hit_rate'] = 1
+        self.arsenal.weapons['grav']['hit_rate'] = 1
+        self.arsenal.weapons['n60']['hit_rate'] = 1
+
+        self.arsenal.weapons['flux']['cooldown'] = .01
+        self.arsenal.weapons['rocket']['cooldown'] = .01
+        self.arsenal.weapons['grav']['cooldown'] = .01
+        self.arsenal.weapons['n60']['cooldown'] = .01
+
     def __str__(self):
         return "botg"

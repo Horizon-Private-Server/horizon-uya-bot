49fb2887b5524e050dd8fef5f8e3e2c1aa0d9481
John Janecek <janecektyler@gmail.com>
2022-04-21 09:23:07 -0600

    Update maps

--- maps/export_point_cloud_to_graph.py
+++ maps/export_point_cloud_to_graph.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 figsize = (12, 12)
-map_name = 'hoven_gorge'
+map_name = 'korgon_outpost'
 display_raw_points = False
 display_downsampled = False
 plot_connected_graph = False

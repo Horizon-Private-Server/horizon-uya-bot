92cfaba19f8d52283faf4d0ad37de0c1704c7df2
John Janecek <janecektyler@gmail.com>
2022-04-26 12:25:17 -0600

    downgrade kisi

--- maps/export_point_cloud_to_graph.py
+++ maps/export_point_cloud_to_graph.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 figsize = (12, 12)
-map_name = 'blackwater_city'
+map_name = 'bakisi_isles'
 display_raw_points = False
 display_downsampled = False
 plot_connected_graph = False

a0c5b782ea6b4727ac6e46e1978132ab2c81c6f5
John Janecek <janecektyler@gmail.com>
2022-06-05 21:16:40 -0600

    remove hyper from docks

--- maps/export_point_cloud_to_graph.py
+++ maps/export_point_cloud_to_graph.py
@@ -8,7 +8,7 @@ import os
 import sys
 
 figsize = (12, 12)
-map_name = 'bakisi_isles'
+map_name = 'blackwater_docks'
 display_raw_points = False
 display_downsampled = False
 plot_connected_graph = False

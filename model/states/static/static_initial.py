2cd549d5c382967ac355f19fd3c888048ce29a45
John Janecek <janecektyler@gmail.com>
2024-12-13 13:58:54 -0700

    fix static initial

--- model/states/static/static_initial.py
+++ model/states/static/static_initial.py
@@ -8,11 +8,12 @@ class static_initial(State):
 
     def enter(self, msg:dict):
         #self.starting_coord = [27205, 54135, 7418] # Marc by flag drop
-        self.starting_coord = [34519, 54146, 7324] # Marc next to red flag
-
+        #self.starting_coord = [34519, 54146, 7324] # Marc next to red flag
+        pass
 
 
     def update(self):
-        self.state_machine.game_state.player.coord = self.starting_coord
-        self.state_machine.target = self.state_machine.game_state.players[0].coord
-        self.state_machine.update_joystick_input_and_angle(self.state_machine.game_state.player.coord, self.state_machine.game_state.player.coord)
+        #self.state_machine.game_state.player.coord = self.starting_coord
+        #self.state_machine.target = self.state_machine.game_state.players[0].coord
+        #self.state_machine.update_joystick_input_and_angle(self.state_machine.game_state.player.coord, self.state_machine.game_state.player.coord)
+        pass

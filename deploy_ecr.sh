8106819de2ab25ff199133c673e8cb0c824e11fe
John Janecek <janecektyler@gmail.com>
2026-01-03 22:10:28 -0700

    update ecr to horizon aws

--- deploy_ecr.sh
+++ deploy_ecr.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 REPO=${1}
 
-#!/bin/bash
+# Export the credentials as environment variables
+export AWS_ACCESS_KEY_ID=$BOT_AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$BOT_AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION="us-west-2"
 
 # Fetch temporary credentials
 TEMP_CREDENTIALS=$(aws sts get-session-token --duration-seconds 3600)
@@ -17,14 +22,14 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN
 
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_SESSION_TOKEN
+echo "Access Key: $AWS_ACCESS_KEY_ID"
+echo "Secret Access: $AWS_SECRET_ACCESS_KEY"
+echo "Session Token: $AWS_SESSION_TOKEN"
 
 
 docker build --platform="linux/amd64" -t ${REPO} .
 
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 062418337237.dkr.ecr.us-west-2.amazonaws.com    
-aws ecr create-repository --repository-name ${REPO} --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
-docker tag ${REPO}:latest 062418337237.dkr.ecr.us-west-2.amazonaws.com/${REPO}:latest
-docker push 062418337237.dkr.ecr.us-west-2.amazonaws.com/${REPO}:latest  
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 579296437268.dkr.ecr.us-west-2.amazonaws.com    
+# aws ecr create-repository --repository-name ${REPO} --image-scanning-configuration scanOnPush=true --image-tag-mutability MUTABLE
+docker tag ${REPO}:latest 579296437268.dkr.ecr.us-west-2.amazonaws.com/${REPO}:latest
+docker push 579296437268.dkr.ecr.us-west-2.amazonaws.com/${REPO}:latest  
